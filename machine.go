8ff3ba91ea572e2819beb3b8f49a22dc420b8818
Kevin Allen <kallen@bostondynamics.com>
2022-12-15 14:49:01 -0500

    gazlle compatiblity

--- machine.go
+++ machine.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	v1 "github.com/juanfont/headscale/gen/go/headscale/v1"
+	v1 "github.com/juanfont/headscale/apigen/go/headscale/v1"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"gorm.io/gorm"
